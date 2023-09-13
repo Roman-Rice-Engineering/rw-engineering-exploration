32e744cc5bfb5804b2304e7a2f9aad739e70b417
Roman-Rice-Engineering <romanrice.engineering@gmail.com>
2023-09-13 13:41:13 -0400

    Added accumulating cookie to index page of api, currently does not work for frontend, EXPERIMENTAL.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,18 @@
 use rocket::{get, routes};
+use rocket::http::{CookieJar, Cookie};
 
 #[get("/")]
-fn index() -> String{
-    "Hello world!".to_owned()
+fn index(cookies: &CookieJar<'_>) -> String{
+    
+    let mut read_cookie: String = match cookies.get("helloworld").map(|c| format!("{}", c.value())){
+        Some(c) => c,
+        None => "Error".to_owned()
+    };
+    read_cookie.push('a');
+    let mut to_ret = "Hello world: ".to_owned();
+    cookies.add(Cookie::new("helloworld", read_cookie.clone()));
+    to_ret.push_str(&read_cookie);
+    to_ret
 }
 
 #[rocket::launch]
