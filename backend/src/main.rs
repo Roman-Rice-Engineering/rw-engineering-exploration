ba9cff32f6509dc4d75c940ec659e119f8cf1d6d
Roman-Rice-Engineering <romanrice.engineering@gmail.com>
2023-09-19 00:14:14 -0400

    Generalized the creation of uuid indexed collection while fixing a naming error.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -25,7 +25,7 @@ async fn rocket() -> _ {
     let db_client = Client::with_uri_str(db_uri).await.expect("unable to connect to database");
     let users = create_users_collection(&db_client).await.expect("unable to create unique index 'username'");
     let sessions = ManySessions::new(db_client.database("auth").collection::<mongodb::bson::Bson>("sessions"));
-    let people = create_people_collection(&db_client).await.expect("unable to create people collection");
+    let people = create_uuid_indexed_collection::<PersonBackend>(&db_client, "people").await.expect("unable to create people collection");
 
     rocket::build()
         .manage(users)
@@ -61,17 +61,17 @@ async fn create_users_collection(db_client: &Client) -> Result<UserCollection, m
     }
 }
 
-async fn create_people_collection(db_client: &Client) -> Result<mongodb::Collection<PersonBackend>, mongodb::error::Error>{
+async fn create_uuid_indexed_collection<T>(db_client: &Client, name: &str) -> Result<mongodb::Collection<T>, mongodb::error::Error>{
     let options = IndexOptions::builder()
         .unique(true)
-        .name("username".to_owned())
+        .name("uuid".to_owned())
         .build();
     let model = IndexModel::builder()
         .keys(mongodb::bson::doc!{"uuid": "text"})
         .options(options)
         .build();
-     match db_client.database("semi-public").collection::<PersonBackend>("people").create_index(model, None).await{
-        Ok(_) => Ok(db_client.database("semi-public").collection::<PersonBackend>("people")),
+     match db_client.database("semi-public").collection::<T>(&name).create_index(model, None).await{
+        Ok(_) => Ok(db_client.database("semi-public").collection::<T>(&name)),
         Err(e) => Err(e)
     }
 }
